9f571f7f5fc1e4c6fab419f77c879bbb7f88bae1
EsdrasAmora <esdras.amora@gmail.com>
2023-09-10 12:34:14 -0300

    temp

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,88 @@ module.exports = {
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
 			}
+		},
+		{
+			rules: {
+				'@typescript-eslint/no-unused-vars': [
+					'error',
+					{
+						argsIgnorePattern: '^_',
+						varsIgnorePattern: '^_',
+						ignoreRestSiblings: true
+					}
+				]
+			}
+		}
+	]
+};
+
+module.exports = {
+	root: true,
+	overrides: [
+		{
+			files: ['*.ts', '*.js'],
+			parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+			plugins: ['@typescript-eslint', 'prettier'],
+			extends: [
+				'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'prettier',
+				'plugin:prettier/recommended'
+			],
+			parserOptions: {
+				ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+				sourceType: 'module', // Allows for the use of imports
+				tsconfigRootDir: __dirname,
+				project: ['./tsconfig.json']
+			},
+			rules: {
+				'prettier/prettier': [
+					'error',
+					{
+						endOfLine: 'auto'
+					}
+				],
+				'func-call-spacing': 'off',
+				'block-scoped-var': 'error',
+				'array-bracket-spacing': ['error', 'never'],
+				curly: ['error', 'all'],
+				'@typescript-eslint/no-namespace': 'off',
+				'@typescript-eslint/require-await': 'off',
+				'@typescript-eslint/no-unnecessary-type-assertion': 'off',
+				'@typescript-eslint/no-non-null-assertion': 'off',
+				'@typescript-eslint/restrict-plus-operands': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/unbound-method': 'off',
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/await-thenable': 'off',
+				'@typescript-eslint/no-use-before-define': 'off',
+				'@typescript-eslint/no-unsafe-assignment': 'off',
+				'@typescript-eslint/no-unsafe-call': 'off',
+				'@typescript-eslint/no-empty-interface': 'off',
+				'@typescript-eslint/no-unsafe-member-access': 'off',
+				'@typescript-eslint/no-unsafe-return': 'off',
+				'@typescript-eslint/no-unsafe-argument': 'off',
+				'@typescript-eslint/no-var-requires': 'off',
+				'@typescript-eslint/no-floating-promises': 'off',
+				'@typescript-eslint/restrict-template-expressions': 'off',
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
+				'@typescript-eslint/member-delimiter-style': [
+					'error',
+					{
+						singleline: {
+							requireLast: false
+						}
+					}
+				]
+			}
+		},
+		{
+			files: ['*.graphql'],
+			parser: '@graphql-eslint/eslint-plugin',
+			plugins: ['@graphql-eslint', 'prettier'],
+			extends: ['prettier', 'plugin:prettier/recommended']
 		}
 	]
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,116 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	type CellVariant = 'mine' | number;
+	type aa = 'flag' | 'closed' | 'open';
+
+	let columns = 8;
+	let rows = 8;
+	let gameOver = false;
+	let won = false;
+	let mines = 10;
+	let board: CellVariant[][] = Array(rows).map(() => Array(columns).fill(0));
+	const deltas = [
+		[0, 1],
+		[1, 0],
+		[1, 1],
+
+		[0, -1],
+		[-1, 0],
+		[-1, -1],
+
+		[1, -1],
+		[-1, 1]
+	];
+
+	function randomInt(max: number) {
+		return Math.round(Math.random() * max);
+	}
+
+	function handleCellPress(i: number, j: number) {
+		const cell = board[i][j];
+		if (cell === 'mine') {
+			gameOver = true;
+			won = false;
+		}
+	}
+
+	while (mines !== 0) {
+		board[randomInt(rows - 1)][randomInt(columns - 1)] = 'mine';
+		mines -= 1;
+		// 💣
+	}
+
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < columns; j++) {
+			if (board[i][j] !== 'mine') {
+				continue;
+			}
+			for (let [x, y] of deltas) {
+				const cell = board[i + x]?.[j + y];
+				if (!cell || cell === 'mine') {
+					continue;
+				}
+				let x = cell;
+				board[i + x][j + y] += 1;
+			}
+		}
+	}
+	mines = 10;
+
+	$: console.log('hello');
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Minesweeper" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>Minesweeper</h1>
+	<div class="board" style="--board-columns: {columns}">
+		{#each board as row, i}
+			{#each row as cell, j}
+				{@const position = i * j}
+				<div
+					class="cell"
+					on:click={() => handleCellPress(i, j)}
+					on:keydown={(e) => {
+						if (e.key === 'Enter') {
+							handleCellPress(i, j);
+						}
+					}}
+					role="cell"
+					tabindex={i + 1}
+				>
+					{cell}
+				</div>
+			{/each}
+		{/each}
+	</div>
 </section>
 
 <style>
+	.board {
+		display: grid;
+		grid-template-columns: repeat(var(--board-columns), auto);
+		background-color: #2196f3;
+	}
+
+	.cell {
+		background-color: rgba(255, 255, 255, 0.8);
+		border: 1px solid rgba(0, 0, 0, 0.8);
+		padding: 20px;
+		font-size: 30px;
+		text-align: center;
+	}
+
+	.cell:hover {
+		background-color: red;
+	}
+
+	.cell:active {
+		background: green;
+	}
+
 	section {
 		display: flex;
 		flex-direction: column;
@@ -39,21 +121,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
